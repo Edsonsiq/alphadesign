41f72b4f65198fddf95b18d676bb42ef769428ad
Edsonsiq <88049071+Edsonsiq@users.noreply.github.com>
2021-07-29 01:12:52 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project-my-favorite-robot-portfolio
+ 
---
+++ main.js
@@ -0,0 +1,10 @@
+const hamburgerMenu = document.querySelector('#navigation .nav-icon');
+const navContent= document.querySelector('#nav-content');
+const closeNavButton = document.querySelector('#nav-content .close-btn');
+
+hamburgerMenu.addEventListener('click', ()=>{
+    navContent.classList.add('show')
+})
+closeNavButton.addEventListener('click', ()=>{
+    navContent.classList.remove('show')
+})
